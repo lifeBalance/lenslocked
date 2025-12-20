b6e17a821b51c8841e9ae48b74f7c3329cf73841
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-20 12:42:21 +0200

    add contact route

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 }
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:sponge@bob.io\">sponge@bob.io</a></p>")
+}
+
 func main() {
 	const PORT string = ":3000"
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on", PORT)
 	http.ListenAndServe(PORT, nil)
 }
