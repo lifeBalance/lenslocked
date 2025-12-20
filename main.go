ceafcbda8ce2aacfdcf3746d38758d04cb7ef284
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-20 19:05:24 +0200

    rudimentary router

--- main.go
+++ main.go
@@ -15,10 +15,24 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:sponge@bob.io\">sponge@bob.io</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "Page not found")
+		// http.Error(w, "Page not found", http.StatusNotFound) // Shorter
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound) // Standard not found message
+	}
+}
+
 func main() {
 	const PORT string = ":3000"
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on", PORT)
 	http.ListenAndServe(PORT, nil)
 }
