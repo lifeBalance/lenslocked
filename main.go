190eba2ff3ae7b586cae6ff37afeece6539934d5
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-20 22:10:32 +0200

    get rid of warnings

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 type Router struct{}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	_ = r
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	_ = r
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:sponge@bob.io\">sponge@bob.io</a></p>")
 }
