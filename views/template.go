108787e6f3ddba7f6548b9c7badbb3f1da568cbb
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-22 21:31:14 +0200

    add variadic support to parse fs

--- views/template.go
+++ views/template.go
@@ -12,8 +12,8 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
 	if err != nil {
 		log.Printf("parsing FS template: %v", err)
 		return Template{}, fmt.Errorf("parsing FS template: %w", err)
