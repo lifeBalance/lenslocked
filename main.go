b32a31fdc52f0f540c34581cbd24f10704f2568c
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-22 14:15:54 +0200

    add controllers

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/lifebalance/lenslocked/controllers"
 	"github.com/lifebalance/lenslocked/views"
 )
 
@@ -21,32 +22,50 @@ func renderTemplate(w http.ResponseWriter, filepath string) {
 	t.Execute(w, nil) // Call a views.Template method
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	_ = r
-	tplPath := filepath.Join("templates", "home.gohtml") // Windows
-	renderTemplate(w, tplPath)
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	_ = r
+// 	tplPath := filepath.Join("templates", "home.gohtml") // Windows
+// 	renderTemplate(w, tplPath)
+// }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	_ = r
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
+// func contactHandler(w http.ResponseWriter, r *http.Request) {
+// 	_ = r
+// 	w.Header().Set("Content-type", "text/html; charset=utf-8")
 
-	tplPath := filepath.Join("templates", "contact.gohtml") // Windows
-	renderTemplate(w, tplPath)
-}
+// 	tplPath := filepath.Join("templates", "contact.gohtml") // Windows
+// 	renderTemplate(w, tplPath)
+// }
 
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	_ = r
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "faq.gohtml") // Windows
-	renderTemplate(w, tplPath)
-}
+// func faqHandler(w http.ResponseWriter, r *http.Request) {
+// 	_ = r
+// 	w.Header().Set("Content-type", "text/html; charset=utf-8")
+// 	tplPath := filepath.Join("templates", "faq.gohtml") // Windows
+// 	renderTemplate(w, tplPath)
+// }
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	// r.Get("/", homeHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	// r.Get("/contact", contactHandler)
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	// r.Get("/faq", faqHandler)
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
