92984ec186a20fb1f5d40cac3ab65a1a386ee868
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-22 13:00:01 +0200

    add views package

--- main.go
+++ main.go
@@ -2,29 +2,23 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/lifebalance/lenslocked/views"
 )
 
 func renderTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath) // Call a package function
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "Error parsing the template", http.StatusInternalServerError)
 		return
 	}
 
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "Error executing the template", http.StatusInternalServerError)
-	}
+	t.Execute(w, nil) // Call a views.Template method
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
---
+++ views/template.go
@@ -0,0 +1,31 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{htmlTpl: tpl}, nil
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+
+	err := t.htmlTpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "Error executing the template", http.StatusInternalServerError)
+	}
+}
