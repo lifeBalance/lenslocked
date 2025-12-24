1cea56fbe23b9c12c39740e738108521320b01a2
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-24 19:46:09 +0200

    add sql injection

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -29,6 +29,8 @@ func main() {
 		Password: "1234",
 		SSLMode:  "disable",
 	}
+
+	// Connecting to db
 	conn, err := pgx.Connect(context.Background(), cfg.Stringify())
 	if err != nil {
 		panic(err)
@@ -38,6 +40,59 @@ func main() {
 	if err := conn.Ping(context.Background()); err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Connected!")
+
+	// Create users table
+	_, err = conn.Exec(context.Background(), `
+	CREATE TABLE IF NOT EXISTS users(
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		email TEXT UNIQUE NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS orders(
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+	);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables Created!")
+
+	// Insert data
+	_, err = conn.Exec(context.Background(), `
+	INSERT INTO users (name, email)
+	VALUES ('Bob', 'bob@test.com');
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User Created!")
+
+	// Another user
+	name := "Liza"
+	email := "liza@test.com"
+	_, err = conn.Exec(context.Background(), `
+	INSERT INTO users (name, email)
+	VALUES ($1, $2);
+	`, name, email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Another user Created!")
+
+	// SQL injection demo ☠️
+	// name := "'',''); DROP TABLE users; --"
+	// email := "haha@test.com"
+	// query := fmt.Sprintf(`
+	// INSERT INTO users (name, email)
+	// VALUES (%s, %s);`, name, email)
+	// _, err = conn.Exec(context.Background(), query)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("You've been Pwned!")
 }
