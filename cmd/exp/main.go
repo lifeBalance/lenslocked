ec4c6b30c3d07bf33b4431a012bacfed63722c0b
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-24 18:03:24 +0200

    connect to pg from go(pgx)

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
+
+	"github.com/jackc/pgx/v5"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) Stringify() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	err := CreateOrg()
-	fmt.Println(err)
-}
-
-func Connect() error {
-	return errors.New("connection failed")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user: %w", err)
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5433",
+		User:     "bob",
+		Database: "lenslocked",
+		Password: "1234",
+		SSLMode:  "disable",
 	}
-	return nil
-}
-
-func CreateOrg() error {
-	err := CreateUser()
+	conn, err := pgx.Connect(context.Background(), cfg.Stringify())
 	if err != nil {
-		return fmt.Errorf("create org: %w", err)
+		panic(err)
 	}
-	return nil
+	defer conn.Close(context.Background())
+
+	if err := conn.Ping(context.Background()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected!")
 }
