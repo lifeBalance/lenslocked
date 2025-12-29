fe82c3855667e22a5fb05fba25430ea0c2edc8e2
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2025-12-29 19:29:59 +0200

    add user to template

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/lifebalance/lenslocked/context"
+	"github.com/lifebalance/lenslocked/models"
 )
 
 type Template struct {
@@ -21,8 +23,10 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	tpl = tpl.Funcs(
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
-				// return `<--! PLACEHOLDER: implement CSRF field in Execute-->`
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (template.HTML, error) {
+				return "", fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -50,6 +54,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 	tpl = tpl.Funcs(template.FuncMap{
 		"csrfField": func() template.HTML {
 			return csrf.TemplateField(r)
+		},
+		"currentUser": func() *models.User {
+			return context.User(r.Context())
 		},
 	})
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
